f1aeb930aca3e7377b099dad70c6fa86aa61f785
Pau Ferrer <MY_NAME@example.compau-ferrer@minimaldesigns.ie>
2023-12-30 22:32:17 +0000

    Getting started!!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { inter } from '../libs/fonts';
+import '../styles/globals.css'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
