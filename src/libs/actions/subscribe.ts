bd1a55aecd7fe93fd5385b49432f035334cbfd5b
Pau Ferrer <MY_NAME@example.compau-ferrer@minimaldesigns.ie>
2024-01-08 10:09:41 +0000

    Subscribe Form FE done

---
+++ src/libs/actions/subscribe.ts
@@ -0,0 +1,67 @@
+'use server'
+// =============================================================================
+// File Name: libs/actions/subscribe.ts
+// File Description:
+// This file contains the code of the Subscribe Form Action.
+// =============================================================================
+// =============================================================================
+// Actions Imports
+// =============================================================================
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+// =============================================================================
+// Actions Form Schemas
+// =============================================================================
+const FormSchema = z.object({
+    subscriberEmail: z.string({
+        required_error: 'Email is required field.'
+    }).email({
+        message: 'Invalid email addres.'
+    }),
+});
+
+// =============================================================================
+// Actions Types
+// =============================================================================
+export type State = {
+    errors?: {
+        subscriberEmail?: string[];
+    };
+    message?: string | null;
+    success?: string | null;
+};
+
+// =============================================================================
+// Actions Functions
+// =============================================================================
+export const handlingSubscriber = async (prevState: State | undefined, formData: FormData) => {
+    
+    // Validating form data
+    const validatedFields = FormSchema.safeParse(
+        Object.fromEntries(formData.entries())
+    )
+
+    // Sending errors if any
+    if(!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing fields. Falied to submit form.',
+        }
+    }
+
+    // Action Processes
+    try {
+        // TODO API call goes here
+        
+        return {
+            success: 'Thanks for subscribing!',
+        }
+        
+    } catch (error) {
+        return {
+            message: 'API ERROR: Failed to perform API call.'
+        }
+    }
+}
--- src/ui/components/form-subscribe.tsx
+++ src/ui/components/form-subscribe.tsx
@@ -1,3 +1,4 @@
+'use client'
 // =============================================================================
 // File Name: ui/components/form-subscribe.tsx
 // File Description:
@@ -6,6 +7,8 @@
 // =============================================================================
 // Components Imports
 // =============================================================================
+import { useFormState } from "react-dom"
+import { handlingSubscriber } from "@/libs/actions/subscribe";
 
 // =============================================================================
 // Components Props
@@ -15,22 +18,54 @@
 // React Components
 // =============================================================================
 export const FormSubscribe = () => {
+
+    // Validation object
+    const initialState = { errors: {}, message: null!, success: null! }
+
+    // Form Action and Error handling
+    const [state, dispatch] = useFormState(handlingSubscriber, initialState);
+
     return (
-        // TODO Set Action for Form Submit
-        // TODO Add validation
-        <form id='form-subscribe' action={''}>
+        <form id='form-subscribe' action={dispatch}>
+
             <div className="mt-4 flex flex-col items-center gap-2 sm:flex-row sm:gap-3 bg-white rounded-[16px] p-2">
-                <div className="w-full">
-                    <label htmlFor="email" className="sr-only">Subscribe</label>
-                    <input type="text" id="email" name="email" className="py-3 px-4 block w-full border-transparent rounded-[8px] text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter your email"/>
+                <div className="w-full relative">
+                    <label htmlFor="subscriberEmail" className="sr-only">Subscribe</label>
+                    <input type="text" id="subscriberEmail" name="subscriberEmail" aria-describedby="subscriberEmail-error" className="py-3 px-4 block w-full border-transparent rounded-[8px] text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter your email"/>
+                    {/* Error Icon */}
+                    {state?.errors?.subscriberEmail ? 
+                        <div className="absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                            <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                            </svg>
+                        </div>
+                        : ''
+                    }
                 </div>
-                <button className="w-full sm:w-auto whitespace-nowrap p-3 inline-flex justify-center items-center gap-x-2 text-white text-xs uppercase font-semibold rounded-[12px] bg-indigo-600 hover:bg-indigo-500 transition-colors duration-[320ms] ease-in-out disabled:opacity-50 disabled:pointer-events-none" type="submit">
+
+                <button type="submit"
+                className="w-full sm:w-auto whitespace-nowrap p-3 inline-flex justify-center items-center gap-x-2 text-white text-xs uppercase font-semibold rounded-[12px] bg-indigo-600 hover:bg-indigo-500 transition-colors duration-[320ms] ease-in-out disabled:opacity-50 disabled:pointer-events-none">
                     Subscribe
                 </button>
+
             </div>
+
+            {/* Errors Block */}
+            <div id='subscriberEmail-error' aria-live="polite" aria-atomic="true">
+                {state?.errors?.subscriberEmail && state?.errors?.subscriberEmail.map((error: string) => <p className="mt-2 text-sm text-red-500" key={error}>{error}</p>)}
+            </div>
+            {/* End Errors Block */}
+
+            {/* Success Block */}
+            <div>
+                {state?.success && <p className="mt-2 text-sm text-green-500">{state?.success}</p>}
+            </div>
+            {/* End Success Block */}
+
             <p className="mt-3 text-sm text-gray-200">
                 New feature releases or big discounts. Never spam 😇
             </p>
+
         </form>
     )
 }
